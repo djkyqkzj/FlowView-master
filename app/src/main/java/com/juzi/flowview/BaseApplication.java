5e0123bb92601e3e272c78d8536172b8a0fcba28
daijingkang <daijingkang@meiyebang.com>
2019-09-17 14:57:47 +0800

    Initial commit

---
+++ app/src/main/java/com/juzi/flowview/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.juzi.flowview;
+
+import android.app.Application;
+
+/**
+ * Created by liangzhongtai on 2017/2/5.
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
